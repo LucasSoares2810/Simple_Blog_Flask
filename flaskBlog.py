ac8e40b44b599a11667d39161ceed9f3c6da51b9
Lucas Soares <108234422+LucasSoares2810@users.noreply.github.com>
2022-11-26 13:56:34 -0300

    Criando uma pagina de registro

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '4dd88b814880612c53b64f4282caaa2a'
 
 posts = [
     {
@@ -27,6 +29,21 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Register', form=form)
 
 
 if __name__ == '__main__':
