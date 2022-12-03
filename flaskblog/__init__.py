8a74d4c1132bedd78f551aa06250b74aacbdf281
Lucas Soares <108234422+LucasSoares2810@users.noreply.github.com>
2022-12-03 13:05:31 -0300

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '4dd88b814880612c53b64f4282caaa2a'
+app.config['SECRET_KEY'] = '????'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
